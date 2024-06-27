b72c4a5ed031ca1cedfd81a6c4624ab4c00fe2d4
AbdelRahmanH53 <boodyhaitham7@gmail.com>
2024-06-27 22:29:58 +0300

    Configured Django

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Django command to wait for database
+"""
+
+from typing import Any
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django cmmand to wait for db"""
+
+    def handle(self, *args, **options) :
+        pass
